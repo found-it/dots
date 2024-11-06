9b4603e183abcf84e72af39d95029da0bc46333f
James Petersen <jpetersenames@gmail.com>
2024-11-06 18:17:31 +0000

    add keygen
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- install.sh
+++ install.sh
@@ -1,59 +1,82 @@
 #!/usr/bin/env bash
 
+function log() {
+  printf 'INFO: %s\n' "${1}"
+}
+
 function tool_exists() {
-  echo "Checking for $1..."
+  log "Checking for $1..."
   which $1 >/dev/null 2>&1 && return 0
   return 1
 }
 
 function directory_exists() {
-  echo "Checking for $1..."
+  log "Checking for $1..."
   test -d $1 >/dev/null 2>&1 && return 0
   return 1
 }
 
 function file_exists() {
-  echo "Checking for $1..."
+  log "Checking for $1..."
   test -f $1 >/dev/null 2>&1 && return 0
+  return 1
+}
+
+function logged_into_github() {
+  gh auth token >/dev/null 2>&1 && return 0
   return 1
 }
 
 # Install nix
 # TODO: do not interact
 if ! tool_exists nix; then
-  echo "Installing nix..."
+  log "Installing nix..."
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
     sh -s -- install --no-confirm
 else
-  echo "nix already exists at $(which nix)"
+  log "nix already exists at $(which nix)"
 fi
 
 # Install tmux plugin manager if it's not there
 DIR=$HOME/.tmux/plugins/tpm
 if ! directory_exists ${DIR}; then
-  echo "Cloning tpm..."
+  log "Cloning tpm..."
   mkdir -p $DIR
   git clone https://github.com/tmux-plugins/tpm $DIR
 else
-  echo "tpm already exists at $DIR"
+  log "tpm already exists at $DIR"
 fi
 
 # Install LazyVim if it's not there
 FILE=$HOME/.config/nvim/lazyvim.json
 if ! file_exists ${FILE}; then
-  PATH=$(dirname $FILE)
-  mkdir -p $PATH
-  mv $PATH{,.bak}
-  git clone https://github.com/LazyVim/starter $PATH
-  rm -rf $PATH/.git
+  LAZYVIM_PATH=$(dirname $FILE)
+  mkdir -p $LAZYVIM_PATH
+  mv $LAZYVIM_PATH{,.bak}
+  git clone https://github.com/LazyVim/starter $LAZYVIM_PATH
+  rm -rf $LAZYVIM_PATH/.git
 else
-  echo "lazyvim already exists at $FILE"
+  log "lazyvim already exists at $FILE"
 fi
 
+# Bootstrap home-manager
+log "Running home-manager switch"
 mkdir -p ~/.config
 ln -sf $PWD/home-manager $HOME/.config/
-
-# TODO: ssh keys
-
-# Bootstrap home-manager
 nix run home-manager/master -- switch
+
+for type in authentication signing; do
+  KEYFILE=$HOME/.ssh/$type
+  if ! file_exists $KEYFILE; then
+    ssh-keygen -t ed25519 -N '' -f $KEYFILE
+
+    if ! logged_into_github; then
+      gh auth login
+    fi
+
+    log "gh ssh-key add $KEYFILE.pub --title "$(hostname)-$type" --type $type"
+    gh ssh-key add $KEYFILE.pub --title "$(hostname)-$type" --type $type
+  else
+    log "$KEYFILE exists"
+  fi
+done
